84e0261c307b8d78f5618f24659e492b13493503
Siri Khalsa <skhalsa10@live.com>
2019-03-20 12:41:40 -0600

    testing a git patch and and merging the Node.java from michelle's branch

--- src/mobileAgents/Node.java
+++ src/mobileAgents/Node.java
@@ -1,18 +1,59 @@
 package mobileAgents;
 
-public class Node {
-    private final Location location;
-    //TODO possible list of edges
+import java.util.ArrayList;
+import java.util.Timer;
+import java.util.TimerTask;
+import java.util.concurrent.LinkedBlockingQueue;
+
+public class Node implements Runnable {
+    //private final Location location;
+    private Location location;
+    private ArrayList<Node> neighbors = new ArrayList<>();
     private State state;
     //TODO possible timer to trigger fire spread
+    private LinkedBlockingQueue<Message> messages = new LinkedBlockingQueue<>();
 
-    public Node(Location location, State state);
+    public Node() {}
 
-    public synchronized setState();
+    public Node(Location location, State state) {
+        this.location = location;
+        this.state = state;
+    }
 
-    public synchronized? sendMessage();
+    //another node constructor without state
+    public Node(Location location) {
+        this.location = location;
+        this.state = State.NOTONFIRE;
+    }
 
-    public spreadFire();
+    public void addNeighbor(Node node) {
+        neighbors.add(node);
+    }
+
+
+    public synchronized void setState(State nextState) {
+        state = nextState;
+        if(state == State.NEARFIRE) {
+            spreadFire();
+        }
+    }
+
+    //public synchronized? sendMessage();
+
+    public synchronized void spreadFire() {
+        Timer timer = new Timer();
+        timer.schedule(new TimerTask() {
+            @Override
+            public void run() {
+                setState(State.ONFIRE);
+                System.out.println("Node at " + location.getX() + " " + location.getY() + " is on fire");
+            }
+        }, 5000);
+    }
+
+    public void processMessage(Message message) throws InterruptedException {
+        messages.put(message);
+    }
 
     //TODO getters
 
@@ -20,5 +61,17 @@ public class Node {
 
 
     public enum State { ONFIRE, NEARFIRE, NOTONFIRE, DEAD}
+
+    @Override
+    public void run(){}
+
+    public static void main (String[] args) {
+        Location location = new Location(0,0);
+        State state = State.NEARFIRE;
+        Node node = new Node(location,state);
+        node.run();
+        node.setState(State.NEARFIRE);
+
+    }
 
 }
