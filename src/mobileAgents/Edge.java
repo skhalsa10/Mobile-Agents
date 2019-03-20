f831f52dfb0d92cafcfdb06790bb15d4d55121dd
mllouie <mllouie@unm.edu>
2019-03-19 22:27:13 -0600

    "Update Edge class with constructor and getters"

--- src/mobileAgents/Edge.java
+++ src/mobileAgents/Edge.java
@@ -1,4 +1,20 @@
 package mobileAgents;
 
 public class Edge {
+    private Location first;
+    private Location second;
+    public Edge (Location first, Location second) {
+        this.first = first;
+        this.second = second;
+    }
+
+    public Location getFirst() {
+        return first;
+    }
+
+    public Location getSecond() {
+        return second;
+    }
+
+
 }
