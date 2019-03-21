06dc4a2da1b9469dc2229f402ae8635704ee684e
mllouie <mllouie@unm.edu>
2019-03-20 22:53:58 -0600

    "Connect nodes and edges for graph"

--- src/mobileAgents/Forest.java
+++ src/mobileAgents/Forest.java
@@ -20,7 +20,6 @@ public class Forest {
             BufferedReader reader = new BufferedReader(new InputStreamReader(in));
             String line;
             while((line = reader.readLine()) != null) {
-                System.out.println(line);
                 readInfo(line);
             }
 
@@ -34,21 +33,17 @@ public class Forest {
         //node info
         if(line.matches("^[nN].*$")) {
             addNode(line);
-            System.out.println("node");
         }
         // edge info
         else if(line.matches("^[eE].*$")) {
             addEdge(line);
-            System.out.println("edge");
         }
         // base info
         else if(line.matches( "^[sS].*$")) {
-            System.out.println("base station");
 
         }
         // fire info
         else if(line.matches("^[fF].*$")) {
-            System.out.println("fire");
         }
 
     }
@@ -76,8 +71,48 @@ public class Forest {
 
     public boolean nodeExists(Location location) {
         for(Node n: forest) {
+            if(location.equals(n.getLocation())) {
+                return true;
+            }
         }
         return false;
+    }
+
+    public void connectNodes(Edge edge) {
+        Location first = edge.getFirst();
+        Location second = edge.getSecond();
+        for(Node n: forest) {
+            if(n.getLocation().equals(first)) {
+                n.addNeighbor(findNode(second));
+            }
+            if(n.getLocation().equals(second)) {
+                n.addNeighbor(findNode(first));
+            }
+        }
+    }
+
+    public Node findNode(Location loc) {
+        for(Node n: forest) {
+            if(n.getLocation().equals(loc)) {
+                return n;
+            }
+        }
+        return null;
+    }
+
+    public void connectGraph() {
+        for(Edge e: edges) {
+            if(nodeExists(e.getFirst()) && nodeExists(e.getSecond())) {
+                connectNodes(e);
+            }
+        }
+    }
+
+    public void printForest() {
+        for(Node n: forest) {
+            n.printNode();
+            n.printNeighbors();
+        }
     }
 
 
@@ -85,6 +120,8 @@ public class Forest {
     public static void main(String[] args) {
         if(args.length > 0) {
             Forest f = new Forest(args[0]);
+            f.connectGraph();
+            f.printForest();
         }
     }
 }
