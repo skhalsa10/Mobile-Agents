7fedff96c30bb5ea8769a2f342df86b867874b66
mllouie <mllouie@unm.edu>
2019-03-23 13:56:22 -0600

    "Add fields and constructor to Agent"

--- src/mobileAgents/Agent.java
+++ src/mobileAgents/Agent.java
@@ -1,13 +1,27 @@
 package mobileAgents;
 
-public class Agent {
-    //UID
-    //Location
-    //possible node...
-    //boolean canWalk
+public class Agent implements Runnable {
+    private final String uid;
+    private Location currentLoc;
+    private Node currentNode;
+    private boolean canWalk;
 
     //makeCopy()
     //walk()
     //sendMessage()
+    public Agent(Location location, Node node, boolean canWalk) {
+        this.uid = "" + location.getX() + "" + location.getY();
+        this.currentLoc = location;
+        this.currentNode = node;
+        this.canWalk = canWalk;
+    }
+
+    public void makeCopy() {
+
+    }
+
+    public void run() {}
+
+
 
 }
