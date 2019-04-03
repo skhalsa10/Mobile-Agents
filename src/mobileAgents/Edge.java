ae4dfbd2bee36735f93564d89cdcf40bd8f85019
mllouie <mllouie@unm.edu>
2019-04-02 22:54:59 -0600

    "Override equals method in edge"

--- src/mobileAgents/Edge.java
+++ src/mobileAgents/Edge.java
@@ -16,5 +16,29 @@ public class Edge {
         return second;
     }
 
+    @Override
+    public boolean equals(Object o) {
+        //null check
+        if(o == null) {
+            return false;
+        }
+
+        //reference check
+        if (o == this) {
+            return true;
+        }
+
+        //instance check
+        if(!(o instanceof Edge)){
+            return false;
+        }
+
+        Edge e = (Edge)o;
+
+        //valueCheck
+        return (this.getFirst().equals(e.getFirst()) &&  this.getSecond().equals(e.getSecond())) ||
+                (this.getFirst().equals(e.getSecond()) && this.getSecond().equals(e.getFirst()));
+    }
+
 
 }
